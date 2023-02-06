f4eb19ec59d2d71bec9439964810522336480285
CallMeAdmiral <thenamesadmiral@gmail.com>
2023-02-06 21:28:47 +0100

    Added styling for done and undone todos

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <main>
     <div>
     <h1>Color mode: {{ $colorMode.value }}</h1>
+    <TheHeader/>
     <select v-model="$colorMode.preference">
       <option value="system">System</option>
       <option value="light">Light</option>
